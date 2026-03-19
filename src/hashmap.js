32d6064fa697553fa09ae561d391be58bce67b1f
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-03-20 03:07:05 +0530

    add resize to hashmap

--- src/hashmap.js
+++ src/hashmap.js
@@ -19,10 +19,13 @@ export class HashMap {
 
   set(key, value) {
     const index = this.hash(key);
+
+    // create bucket if not exist
     if (!this.buckets[index]) {
       this.buckets[index] = [];
     }
 
+    // check if key already exist in bucket
     const bucket = this.buckets[index];
     for (let i = 0; i < bucket.length; i++) {
       if (bucket[i].key === key) {
@@ -31,7 +34,34 @@ export class HashMap {
       }
     }
 
+    // add new key-value pair
     bucket.push({ key, value });
     this.size++;
+
+    // check if resize is needed after adding new key-value pair
+    if (this.size / this.capacity > this.loadFactor) {
+      this.resize();
+    }
+  }
+
+  resize() {
+    const oldBuckets = this.buckets; //saving old array data
+
+    this.capacity *= 2; //doubling the capacity
+    this.buckets = new Array(this.capacity).fill(null); //creating new array with new capacity
+
+    this.size = 0; //resetting size
+
+    //copying old data to new array
+    oldBuckets.forEach(bucket => {
+      //checking if bucket is not empty
+      if (bucket !== null) {
+        //iterating through each pair in the bucket
+        bucket.forEach(pair => {
+          //setting the pair in the new array using set method
+          this.set(pair.key, pair.value);
+        })
+      }
+    })
   }
 }
