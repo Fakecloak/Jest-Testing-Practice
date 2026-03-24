04cfaf02082d18508a4c797dc148298d0adcff64
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-03-24 20:43:40 +0530

    add node and Binary class

---
+++ src/bst.js
@@ -0,0 +1,35 @@
+class node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = this.buildTree();
+    }
+
+    buildTree(arr) {
+        // sort the array
+        const sortedArr = [...new Set(arr.sort((a, b) => a - b))];
+
+        //helper recursive function to build the tree
+        const build = (arr) => {
+
+            if (arr.length === 0) return null; // base case
+
+            const mid = Math.floor(arr.length / 2); // find middle index
+            const root = new node(arr[mid]) // Create a node using middle value → this becomes root
+
+            root.left = build(arr.slice(0, mid)); // recursively build left subtree
+            root.right = build(arr.slice(mid + 1)); // recursively build right subtree
+
+            return root;
+        }
+
+        return build(sortedArr);
+    }
+}
+
